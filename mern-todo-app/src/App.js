2798433ac7da236af907487bd22dcfa8d67a7404
Malik Shalash <mjshalash@rci4u.com>
2019-03-18 10:05:33 -0400

    Finished basic tutorial
    Continuing learning on udemy

--- mern-todo-app/src/App.js
+++ mern-todo-app/src/App.js
@@ -1,16 +1,35 @@
-import React, { Component } from 'react';
-import { BrowserROuter as Router, Route, Link } from 'react-router-dom';
-import "bootstrap/dist/css/bootstrap.min.css"
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component";
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <Route path="/" exact component={TodosList} />
-        <Route path="/edit/id" exact component={TodosList} />
-        <Route path="/create" exact component={TodosList} />
         <div className="container">
-          <h2>MERN-Stack Todo App</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="https://codingthesmartway.com" target="_blank">
+            </a>
+            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Todos</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create Todo</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <br/>
+          <Route path="/" exact component={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
         </div>
       </Router>
     );
